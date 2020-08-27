b1229a5c12745c7673a91382e9668e5f6b7a3f33
SandyOliveira <sandyoliveira828@gmail.com>
2020-08-27 16:42:23 -0300

    Autenticacao

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,36 +5,40 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
-    return <Redirect to="/" />
+    return <Redirect to="/" />;
   }
   if (signed && !isPrivate) {
-    return <Redirect to="/dashboard" />
+    return <Redirect to="/dashboard" />;
   }
 
   const Layout = signed ? DefaultLayout : AuthLayout;
 
   return (
-    <Route {...rest} render={props => (
-      <Layout>
-        <Component {...props} />
-      </Layout>
-    )}
-
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
     />
   );
 }
 
 RouteWrapper.prototype = {
   isPrivate: PropTypes.bool,
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
 };
 
 RouteWrapper.defaultProps = {
